c3d3a9ec4edea368030320fbd8469b577d0035a0
Thomas Whitcomb <thomas.whitcomb04@gmail.com>
2026-01-26 12:17:36 -0800

    Update PipeSpawnerScript with comments for code clarity

--- Assets/PipeSpawnerScript.cs
+++ Assets/PipeSpawnerScript.cs
@@ -2,35 +2,38 @@ using UnityEngine;
 
 public class PipeSpawnerScript : MonoBehaviour
 {
+    //GameObject Reference
     public GameObject pipePrefab;
+
+    //Pipe Spawner Variables
     public float spawnRate = 2.5f;
-    private float timer = 0f;
     public float verticalOffset = 10f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
+    private float spawnTimer = 0f;
+    
     // Update is called once per frame
     void Update()
     {
-        if (timer < spawnRate)
+        //Check if spawnTimer has elapsed
+        if (spawnTimer < spawnRate) //If not, continue timer
         {
-            timer += Time.deltaTime;
+            spawnTimer += Time.deltaTime;
         }
-        else
+        else //If timer is up
         {
-            SpawnPipe();
-            timer = 0f;
+            SpawnPipe(); //Spawn Pipe
+            spawnTimer = 0f; //Reset pipe spawnTimer
         }
     }
+
+    //Method to randomly generate pipe between vertical offset bounds
     private void SpawnPipe()
     {
+        //Vertial Offset Lower and Upper Bounds
         float lowestPoint = transform.position.y - verticalOffset;
         float highestPoint = transform.position.y + verticalOffset;
 
+        //Create new pipe GameObject between given lower and upper bounds
         Instantiate(pipePrefab, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint),transform.position.z), transform.rotation);
     }
 }
+
